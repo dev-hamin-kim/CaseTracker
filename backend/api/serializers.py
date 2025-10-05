016e54840a4df90b053453c714ad93f44ee9fe89
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-10-05 17:10:46 +0900

    [fix] fixed weird bug that prevented getting tokens

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,10 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "password"]
-        extra_kwargs = {
-            "password":{"write_only": True}
-            }
+        extra_kwargs = {"password": {"write_only": True}}
 
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            return user
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
