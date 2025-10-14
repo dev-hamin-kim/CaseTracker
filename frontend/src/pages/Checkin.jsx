c860f9f01a4fa41971ac69dbb6c1c2f857448ba4
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-10-15 08:59:29 +0900

    [feat] created Checkin.jsx file and moved the component

---
+++ frontend/src/pages/Checkin.jsx
@@ -0,0 +1,32 @@
+import { Asset, Top, FixedBottomCTA } from '@toss/tds-mobile';
+import { Spacing } from '@toss/emotion-utils';
+import { adaptive } from '@toss/tds-colors';
+
+export default function Checkin() {
+  return (
+    <>
+      <Spacing size={14} />
+      <Top
+        title={
+          <Top.TitleParagraph size={22} color={adaptive.grey900}>
+            케이스 진행도를 더 쉽게 파악해요.
+          </Top.TitleParagraph>
+        }
+        subtitleBottom={
+          <Top.SubtitleParagraph>
+            업무를 보다 쉽게 파악해요.
+          </Top.SubtitleParagraph>
+        }
+      />
+      <Spacing size={98} />
+      <>
+        <Asset.Image
+          frameShape={{ width: 200 }}
+          src="https://static.toss.im/icons/png/4x/menu-list.png"
+          aria-hidden={true}
+        />
+      </>
+      <FixedBottomCTA loading={false}>출근하기</FixedBottomCTA>
+    </>
+  );
+}
