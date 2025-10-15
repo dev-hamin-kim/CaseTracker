bc30723be12058c4dac7384f869ffcaf18e905ba
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-10-15 09:00:30 +0900

    [feat] added stuffs for routing and removed duplicated codes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,12 @@
-import {
-  // assetFrameShape,
-  // AssetIcon,
-  // AssetContentIcon,
-  Asset,
-  // Text,
-  // AssetImage,
-  // AssetContentImage,
-  Top,
-  // TopTitleParagraph,
-  // TopSubtitleParagraph,
-  FixedBottomCTA,
-} from '@toss/tds-mobile';
-import { Spacing } from '@toss/emotion-utils';
-import { adaptive } from '@toss/tds-colors';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Checkin from "./pages/Checkin";
 
 export default function App() {
   return (
-    <>
-      <Spacing size={14} />
-      <Top
-        title={
-          <Top.TitleParagraph size={22} color={adaptive.grey900}>
-            케이스 진행도를 더 쉽게 파악해요.
-          </Top.TitleParagraph>
-        }
-        subtitleBottom={
-          <Top.SubtitleParagraph>
-            업무를 보다 쉽게 파악해요.
-          </Top.SubtitleParagraph>
-        }
-      />
-      <Spacing size={98} />
-      <>
-        <Asset.Image
-          frameShape={{ width: 200 }}
-          src="https://static.toss.im/icons/png/4x/menu-list.png"
-          aria-hidden={true}
-        />
-      </>
-      <FixedBottomCTA loading={false}>출근하기</FixedBottomCTA>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Checkin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
