1c4c279bf59491fe2d4bbdeb4fbf481b860a9791
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-10-16 19:36:49 +0900

    [feat] created urls for Devices

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("cases/", views.CaseListCreate.as_view(), name="case-list"),
-    path("cases/delete/<int:pk>/", views.CaseDelete.as_view(), name="delete-case")
+
+    # ===== CASES URLS ======
+    path("cases/", views.CaseList.as_view(), name="case-list"),
+    path("cases/create/", views.CaseListCreate.as_view(), name="create-case"),
+    path("cases/delete/<int:pk>/", views.CaseDelete.as_view(), name="delete-case"),
+
+    # ===== DEVICE URLS ======
+    path("devices/create/", views.DeviceListCreate.as_view(), name="create-device"),
+    path("devices/", views.DeviceList.as_view(), name="device-list"),
 ]
