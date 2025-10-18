5b0f73f9b7c9095eeb25a64e2b9a52d5f744ab3f
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-10-18 12:57:34 +0900

    [chore] switched to custom user models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
 
 class Case(models.Model):
     name = models.CharField(max_length=200)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,6 +20,7 @@ load_dotenv(find_dotenv())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+AUTH_USER_MODEL = "api.User"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
