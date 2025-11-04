0210af4192e55bb03d8d45ae755540b5116f6843
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-11-05 08:51:04 +0900

    [chore] added models to django admin page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User, Case, Device, Variant
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Case)
+class CaseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Variant)
+class VariantAdmin(admin.ModelAdmin):
+    pass
