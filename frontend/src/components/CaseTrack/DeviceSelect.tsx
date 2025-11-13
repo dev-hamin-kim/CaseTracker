3560890cf9d2f82fc595778d4d4fbb8906a1da12
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2025-11-13 18:43:35 +0900

    [feat] added CTA buttons in DeviceSelect modal
    for closing and unselecting the selection

--- frontend/src/components/CaseTrack/DeviceSelect.tsx
+++ frontend/src/components/CaseTrack/DeviceSelect.tsx
@@ -1,21 +1,37 @@
 import React from "react";
-import { BottomSheet } from "@toss/tds-mobile";
+import { BottomSheet, Button } from "@toss/tds-mobile";
 
 interface Props {
   devices: string[];
   onCheck: (checked: string) => void;
   isOpen: boolean;
+  onClose: () => void;
 }
 
-export function DeviceSelect({ devices, onCheck, isOpen }: Props) {
+export function DeviceSelect({ devices, onCheck, isOpen, onClose }: Props) {
   const [checked, setChecked] = React.useState<string | null>(null);
 
   return (
     <>
       <BottomSheet
         open={isOpen}
+        // onClose={onClose}
         header={
-          <BottomSheet.Header>테스트할 기기를 선택해주세요.</BottomSheet.Header>
+          <BottomSheet.Header>디바이스를 선택해주세요.</BottomSheet.Header>
+        }
+        cta={
+          <BottomSheet.DoubleCTA
+            leftButton={
+              <Button variant="weak" color="danger" onClick={() => onCheck("")}>
+                선택 해제하기
+              </Button>
+            }
+            rightButton={
+              <Button variant="weak" color="dark" onClick={onClose}>
+                닫기
+              </Button>
+            }
+          ></BottomSheet.DoubleCTA>
         }
       >
         <BottomSheet.Select
